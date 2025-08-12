89ea144f6a73034791edde9febadcaa4f2e8b6d8
45337 <144039593+45337@users.noreply.github.com>
2025-08-12 20:42:20 +0200

    Update setup_fileshare.sh

--- setup_fileshare.sh
+++ setup_fileshare.sh
@@ -5,6 +5,7 @@ vmware-hgfsclient | while read folder; do
   mkdir -p "/mnt/hgfs/${folder}"
   umount -f "/mnt/hgfs/${folder}" 2>/dev/null
   vmhgfs-fuse -o allow_other -o auto_unmount ".host:/${folder}" "/mnt/hgfs/${folder}"
+  cp -s -r /mnt/hgfs/${folder} /home/kali/Desktop
 done
 
 sleep 2s
