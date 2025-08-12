186ace5df57f6ecbca25ddd59f61dfa418d46e84
45337 <144039593+45337@users.noreply.github.com>
2025-08-12 19:44:07 +0200

    Add files via upload

---
+++ setup kali.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+
+sudo sed -i 's/us/de/' /etc/default/keyboard
+
+sudo apt update && sudo apt upgrade
+
+sudo apt install seclists
+
+wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+
+mv rockyou.txt /usr/share/wordlists/
+
+sudo bash ./setup_fileshare.sh
---
+++ setup_whonix.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+
+nmcli c modify Wired\ connection\ 1 ipv4.addresses 10.152.152.11/18
+nmcli c modify Wired\ connection\ 1 ipv4.method manual
+nmcli c modify Wired\ connection\ 1 ipv4.dns 10.152.152.10
+nmcli c modify Wired\ connection\ 1 ipv4.gateway 10.152.152.10
+
+sudo ip route add default via 10.152.152.10 dev eth0
